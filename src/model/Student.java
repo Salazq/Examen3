6534b0cc9143048596d8f9e8433f2d9b1b157d0f
Salazq <jc.salazarquintero@gmail.com>
2022-05-18 09:27:52 -0500

    Init

---
+++ src/model/Student.java
@@ -0,0 +1,110 @@
+package model;
+
+
+public class Student {
+
+    private String name;
+    private String id;
+    private int age;
+    private double average;
+    private NoteSubject [] notes;
+    private String status;
+
+    public Student(String name, String id, int age, NoteSubject[] notes){
+
+        this.name=name;
+        this.age=age;
+        this.id=id;
+        this.notes= notes;
+        average=-1;
+    }
+
+    public void addSubject(NoteSubject subject, int pos){
+        notes [pos]=subject;
+    }
+
+    public String  showSubjects(){
+
+        String [][]notesTable;
+        String out="";
+
+		notesTable= new String[4][1+notes.length];
+
+        notesTable[0][0]="      ";
+        notesTable[1][0]="Exam 1";
+        notesTable[2][0]="Exam 2";
+        notesTable[3][0]="Exam 3";
+
+		for (int i=1; i<=(notes.length); i++){
+
+			notesTable[0][i] = notes[i-1].getNameSubject();
+            notesTable[1][i] = ""+notes[i-1].getValueNote1();
+            notesTable[2][i] = ""+notes[i-1].getValueNote2();
+            notesTable[3][i] = ""+notes[i-1].getValueNote3();
+		}
+
+		if (notes.length!=0){
+			out = printString(notesTable);
+		}
+        return out;
+    }
+
+    private String printString(String[][] matrix) {
+		String print ="";
+		for (int i=0; i< matrix.length; i++ ) { // filas numbers.length
+			for (int j = 0; j < matrix[0].length; j++) { //columnas numbers[0].length
+				print += matrix[i][j] + " ";
+			}
+			print += "\n";
+		}
+		return print;
+	}
+
+    public double calculateAverage(){
+
+        double noteAverage=0;
+        int credits=0;
+
+        for (int i=0; i<notes.length; i++){
+
+            noteAverage+=((notes[i].getValueNote1()+ notes[i].getValueNote2()+ notes[i].getValueNote3())/3)* notes[i].getCreditsSubject();
+            credits+= notes[i].getCreditsSubject();
+        }
+        double total=(double)Math.round((noteAverage/credits)*100)/100;
+
+        return total;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getAge(){
+        return ""+age;
+    }
+
+    public String getAverage(){
+        return ""+ calculateAverage();
+    }
+
+    public String toString (){
+
+        if (average==-1){
+
+            average=calculateAverage();
+
+            if (average<3.5){
+
+                status="Reprobate";
+            }
+            else {
+                status="Aprobate";
+            }
+        }
+        return "\nName: "+ name +"\nage: "+ age +"\naverage: "+ average+"\nStatus: "+ status + "\n\n"+ showSubjects();
+    }
+}
---
+++ src/ui/Init.java
@@ -0,0 +1,66 @@
+package ui;
+import model.*;
+
+public class Init {
+
+    public Init() { }
+
+    public Student[][] initStudents(){
+
+        Student[][] students=new Student [10][10];
+
+        NoteSubject[] notes1= new NoteSubject[1];
+        notes1[0]= new NoteSubject(3, 4, 5, "APO", 2);
+
+        students=addStudent(students, " Camilo ", "123", 18, notes1);
+
+        NoteSubject[] notes2= new NoteSubject[2];
+        notes2[0]= new NoteSubject(2, 2, 4, "APO", 2);
+        notes2[1]= new NoteSubject(2, 5, 4, "MATH", 3);
+
+        students=addStudent(students, "Santiago", "456", 2, notes2);
+
+        NoteSubject[] notes3= new NoteSubject[3];
+        notes3[0]= new NoteSubject(1, 4, 3, "APO", 2);
+        notes3[1]= new NoteSubject(3, 3, 4, "MATH", 3);
+        notes3[2]= new NoteSubject(5, 5, 4, "COE", 2);
+
+        students=addStudent(students, " Nelson ", "789", 3, notes3);
+
+    
+        return students;
+    }
+
+    public int []emptypos(Student[][] students){
+
+		int [] pos= new int[2];
+
+		boolean found=false;
+
+		for (int i=0; i<10 && found==false; i++){
+
+			for (int e=0; e<10&& found==false; e++){
+
+				if (students[i][e]==null){
+
+					found=true;
+					pos[0]=i;
+					pos[1]=e;
+				}
+			}
+		}
+		return pos;
+	}
+
+	public Student[][] addStudent(Student[][] students,String name, String id, int age, NoteSubject[] notes) {
+		
+		Student newStudent= new Student(name, id, age, notes);
+		int [] pos=emptypos(students);
+
+		students [pos[0]][pos[1]]= newStudent;
+
+        return students;
+	}
+
+
+}
